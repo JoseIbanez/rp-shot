dface326c730823d5cc0834400999ab29a4cea43
Jose Ibanez <ibanez.j@gmail.com>
2019-05-12 22:53:02 +0200

    tl

---
+++ capture.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "go home"
+cd /home/ibanez/Projects/rp-shot/
+
+echo "Turn on the lights"
+mosquitto_pub -h 192.168.1.19 -t  "q/ESP30AEA4283B74" -m "0060;0001"
+sleep 5
+
+#d5300 config
+echo "Configure D5300"
+sudo gphoto2 --camera "Nikon DSC D5300" --set-config /main/capturesettings/imagequality=2
+sudo gphoto2 --camera "Nikon DSC D5300" --set-config /main/capturesettings/flashmode=0
+
+
+cd /mnt/d2/tl/201905-Albahaca
+echo "Capture D5300"
+sudo gphoto2 --capture-image-and-download --camera "Nikon DSC D5300"        --filename 'd5300-%Y%m%d-%H%M%S.jpg'
+
+echo "Capture A300"
+sudo gphoto2 --capture-image-and-download --camera "USB PTP Class Camera"   --filename 'a300-%Y%m%d-%H%M%S.jpg'
+
+echo "Other light"
+mosquitto_pub -h 192.168.1.19 -t  "q/ESP30AEA4283B74" -m "0030;0010"
+sleep 1
+
+echo "Webcam"
+name=`date  +webcam-%Y%m%d-%H%M%S.jpg`
+fswebcam -r 1920x1080 $name
+
+
+mosquitto_pub -h 192.168.1.19 -t  "q/ESP30AEA4283B74" -m "0000;0000"
+
+
---
+++ twitter_update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -f /tmp/*.jpg
+
+ofile=`find /mnt/d2/tl/201905-Albahaca/ -mmin -30 -name "a300*" | tail -n 1`
+convert $ofile -quality 80 /tmp/a300.jpg
+
+ofile=`find /mnt/d2/tl/201905-Albahaca/ -mmin -30 -name "d5300*" | tail -n 1`
+convert $ofile -quality 80 /tmp/d5300.jpg
+
+ls -lh /tmp/a300.jpg
+ls -lh /tmp/d5300.jpg
+
+
+./twitter_upload.py -text "TL A300 Albahaca" -file  /tmp/a300.jpg
+sleep 10
+./twitter_upload.py -text "TL D5300 Albahaca" -file  /tmp/d5300.jpg
+
