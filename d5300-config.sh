bb42f49e813c2d8e1fac25317b24b713951794fb
Jose Ibanez <ibanez.j@gmail.com>
2019-05-12 16:16:50 +0000

    d5300

--- d5300-config.sh
+++ d5300-config.sh
@@ -1,10 +1,12 @@
-
+#
 gphoto2 --list-config
   /main/capturesettings/imagequality
 
+# to Jpg
 gphoto2 --get-config /main/capturesettings/imagequality
-
 gphoto2 --set-config /main/capturesettings/imagequality=2
 
-
+# to No Flash
+gphoto2 --get-config /main/capturesettings/flashmode
+gphoto2 --set-config /main/capturesettings/flashmode=0
 
