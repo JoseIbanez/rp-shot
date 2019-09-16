44ad5c54828a20ce0145c9f5193dbbd389a40a14
Jose Ibanez <ibanez.j@gmail.com>
2019-09-16 19:59:16 +0200

    myPino

--- capture.sh
+++ capture.sh
@@ -4,7 +4,8 @@ echo "go home"
 cd /home/ibanez/Projects/rp-shot/
 
 old=""
-topic="q/ESP3C71BF51A064"
+#topic="q/ESP3C71BF51A064"
+topic="q/ESP60019440E65A"
 
 echo "Turn on the lights"
 #mosquitto_pub -h 192.168.1.19 -t  "q/ESP30AEA4283B74" -m "0060;0001"
@@ -13,7 +14,7 @@ mosquitto_pub -h 192.168.1.19 -t  $topic -m "0001;1111"
 sleep 2
 mosquitto_pub -h 192.168.1.19 -t  $topic -m "0001;1111"
 sleep 2
-mosquitto_pub -h 192.168.1.19 -t  $topic -m "0060;0001"
+mosquitto_pub -h 192.168.1.19 -t  $topic -m "0060;1111"
 sleep 5
 
 
@@ -23,20 +24,20 @@ sudo gphoto2 --camera "Nikon DSC D5300" --set-config /main/capturesettings/image
 sudo gphoto2 --camera "Nikon DSC D5300" --set-config /main/capturesettings/flashmode=0
 
 
-cd /mnt/d2/tl/201905-Albahaca
+cd /mnt/d2/tl/201909-Pino
 echo "Capture D5300"
 sudo gphoto2 --capture-image-and-download --camera "Nikon DSC D5300"        --filename 'd5300-%Y%m%d-%H%M%S.jpg'
 
-echo "Capture A300"
-sudo gphoto2 --capture-image-and-download --camera "USB PTP Class Camera"   --filename 'a300-%Y%m%d-%H%M%S.jpg'
+#echo "Capture A300"
+#sudo gphoto2 --capture-image-and-download --camera "USB PTP Class Camera"   --filename 'a300-%Y%m%d-%H%M%S.jpg'
 
-echo "Other light"
-mosquitto_pub -h 192.168.1.19 -t  $topic -m "0030;0010"
-sleep 1
+#echo "Other light"
+#mosquitto_pub -h 192.168.1.19 -t  $topic -m "0030;0010"
+#sleep 1
 
-echo "Webcam"
-name=`date  +webcam-%Y%m%d-%H%M%S.jpg`
-fswebcam -r 1920x1080 $name
+#echo "Webcam"
+#name=`date  +webcam-%Y%m%d-%H%M%S.jpg`
+#fswebcam -r 1920x1080 $name
 
 
 mosquitto_pub -h 192.168.1.19 -t $topic -m "0000;0000"
