7c77d443c6b33c6ea269b8a26b305b652054c4fd
Jose Ibanez <ibanez.j@gmail.com>
2020-04-01 17:03:29 +0000

    rm capture

--- capture.sh
+++ capture.sh
@@ -5,7 +5,8 @@ cd /home/ibanez/Projects/rp-shot/
 
 old=""
 #topic="q/ESP3C71BF51A064"
-topic="q/ESP60019440E65A"
+#topic="q/ESP60019440E65A"
+topic="q/ESP2C3AE8128E75"
 
 echo "Turn on the lights"
 #mosquitto_pub -h 192.168.1.19 -t  "q/ESP30AEA4283B74" -m "0060;0001"
@@ -28,8 +29,8 @@ cd /mnt/d2/tl/201911-Encina
 echo "Capture D5300"
 sudo gphoto2 --capture-image-and-download --camera "Nikon DSC D5300"        --filename 'd5300-%Y%m%d-%H%M%S.jpg'
 
-#echo "Capture A300"
-#sudo gphoto2 --capture-image-and-download --camera "USB PTP Class Camera"   --filename 'a300-%Y%m%d-%H%M%S.jpg'
+echo "Capture A300"
+sudo gphoto2 --capture-image-and-download --camera "USB PTP Class Camera"   --filename 'a300-%Y%m%d-%H%M%S.jpg'
 
 #echo "Other light"
 #mosquitto_pub -h 192.168.1.19 -t  $topic -m "0030;0010"
--- twitter_update.sh
+++ twitter_update.sh
@@ -6,8 +6,8 @@ cd $scriptHome
 
 rm -f /tmp/*.jpg
 
-#ofile=`find /mnt/d2/tl/201905-Albahaca/ -mmin -30 -name "a300*" | tail -n 1`
-#convert $ofile -quality 80 /tmp/a300.jpg
+ofile=`find /mnt/d2/tl/201911-Encina/ -mmin -30 -name "a300*" | tail -n 1`
+convert $ofile -quality 80 /tmp/a300.jpg
 
 ofile=`find /mnt/d2/tl/201911-Encina/ -mmin -30 -name "d5300*" | tail -n 1`
 convert $ofile -quality 80 /tmp/d5300.jpg
@@ -16,7 +16,7 @@ ls -lh /tmp/a300.jpg
 ls -lh /tmp/d5300.jpg
 
 
-#./twitter_upload.py -text "TL A300 Albahaca" -file  /tmp/a300.jpg
-#sleep 10
-./twitter_upload.py -text "TL D5300 Albahaca" -file  /tmp/d5300.jpg
+./twitter_upload.py -text "TL A300 Encina" -file  /tmp/a300.jpg
+sleep 10
+./twitter_upload.py -text "TL D5300 Roble" -file  /tmp/d5300.jpg
 
