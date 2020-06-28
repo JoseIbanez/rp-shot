7749c6f572d417e14727c5af1bfa9ea875d74334
Jose Ibanez <ibanez.j@gmail.com>
2020-06-28 19:12:46 +0000

    detect missing camera

--- h2-capture.sh
+++ h2-capture.sh
@@ -24,28 +24,56 @@ mosquitto_pub -h 192.168.1.19 -t  $topic -m "0060;1111"
 #sudo gphoto2 --camera "Nikon DSC D5300" --set-config /main/capturesettings/imagequality=2
 #sudo gphoto2 --camera "Nikon DSC D5300" --set-config /main/capturesettings/flashmode=0
 
+# Serial Numbers 
+lsusb -v -d 04b0:0363 | grep -B 20 'iSerial' | grep -e iSerial -e "Bus.*Device"
 
 
-photoPath="/mnt/d2/tl/202004-Tomate"
+photoPath="/mnt/d2/tl/202006-Aguacate2"
 
 mkdir -p $photoPath
 cd $photoPath
 
 
-
 #echo "Capture D5300"
 #sudo gphoto2 --capture-image-and-download --camera "Nikon DSC D5300"        --filename 'd5300-%Y%m%d-%H%M%S.jpg'
 
-echo "Capture A300"
-sudo gphoto2 --capture-image-and-download --camera "USB PTP Class Camera"   --filename 'a300-%Y%m%d-%H%M%S.jpg'
+#echo "Capture A300"
+#sudo gphoto2 --capture-image-and-download --camera "USB PTP Class Camera"   --filename 'a300-%Y%m%d-%H%M%S.jpg'
+
+echo "Capture A300 - 1 Back"
+pos=`lsusb -v -d 04b0:0363 | grep -B 20 'iSerial.*000041066942' | grep "Bus.*Device.*Nikon " | sed 's/.*Device \([0-9]*\): ID.*/\1/'`
+echo $pos
+if [ ! -z "$pos" ]; then
+  sudo gphoto2 --capture-image-and-download --port usb:001,$pos --filename 'a300-%Y%m%d-%H%M%S.jpg'
+fi
+
+#################
+
+photoPath="/mnt/d2/tl/202006-Aguacate3"
+mkdir -p $photoPath
+cd $photoPath
+
+echo "Capture A300 - 2 Red"
+pos=`lsusb -v -d 04b0:0363 | grep -B 20 'iSerial.*000043009524' | grep "Bus.*Device.*Nikon " | sed 's/.*Device \([0-9]*\): ID.*/\1/'`
+echo $pos
+if [ ! -z "$pos" ]; then
+  sudo gphoto2 --capture-image-and-download --port usb:001,$pos --filename 'a300-%Y%m%d-%H%M%S.jpg'
+fi
+
+#######################
 
 #echo "Other light"
 #mosquitto_pub -h 192.168.1.19 -t  $topic -m "0030;0010"
 #sleep 1
 
-#echo "Webcam"
+photoPath="/mnt/d2/tl/202006-Room"
+mkdir -p $photoPath
+cd $photoPath
+
+echo "Webcam"
 name=`date  +webcam-%Y%m%d-%H%M%S.jpg`
-fswebcam -r 1920x1080 $name
+sudo fswebcam -r 1920x1080 $name
+
 
 
 mosquitto_pub -h 192.168.1.19 -t $topic -m "0000;0000"
--- twitter_update.sh
+++ twitter_update.sh
@@ -6,8 +6,11 @@ cd $scriptHome
 
 rm -f /tmp/*.jpg
 
+title1="TL A300 Aguacate"
+path1="/mnt/d2/tl/202006-Aguacate3"
 
-ofile=`find /mnt/d2/tl/201911-Nuez/ -mmin -30 -name "a300*" | tail -n 1`
+
+ofile=`find "$path1" -mmin -30 -name "a300*" | tail -n 1`
 convert $ofile -quality 80 /tmp/a300.jpg
 
 ofile=`find /mnt/d2/tl/201911-Nuez/ -mmin -30 -name "d5300*" | tail -n 1`
@@ -17,7 +20,7 @@ ls -lh /tmp/a300.jpg
 ls -lh /tmp/d5300.jpg
 
 
-./twitter_upload.py -text "TL A300 Encina" -file  /tmp/a300.jpg
+./twitter_upload.py -text "$title1" -file  /tmp/a300.jpg
 sleep 10
 ./twitter_upload.py -text "TL D5300 Roble" -file  /tmp/d5300.jpg
 
